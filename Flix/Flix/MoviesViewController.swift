49e560ca090ab3285bf9f37bc0b28212397afcbb
quangcito <qnguyen2@macalester.edu>
2022-09-12 20:37:55 -0500

    add project to github

---
+++ Flix/Flix/MoviesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Nguyễn Đương Quang on 9/12/22.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
