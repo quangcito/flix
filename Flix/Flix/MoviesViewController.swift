30762973c01710cd28b64150a3f10e70d26e62b9
quangcito <qnguyen2@macalester.edu>
2022-09-12 21:09:30 -0500

    finish table view setup

--- Flix/Flix/MoviesViewController.swift
+++ Flix/Flix/MoviesViewController.swift
@@ -7,12 +7,18 @@
 
 import UIKit
 
-class MoviesViewController: UIViewController {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    
+    @IBOutlet weak var tableView: UITableView!
     
     var movies = [[String:Any]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view.
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
@@ -27,6 +33,8 @@ class MoviesViewController: UIViewController {
 
                     // TODO: Get the array of movies
                     self.movies = dataDictionary["results"] as! [[String:Any]]
+                    
+                    self.tableView.reloadData()
                     print(dataDictionary)
                  
                     // TODO: Store the movies in a property to use elsewhere
@@ -37,6 +45,20 @@ class MoviesViewController: UIViewController {
         task.resume()
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        
+        cell.textLabel!.text = title
+        
+        return cell
+    }
 
 }
 
